88df5e805c53d7b2ff31c68d1dfd53cd49da844a
alfons03 <mario.orozco.pacheco@gmail.com>
2023-09-28 10:50:36 -0500

    información

--- Server/api-courses/tecnologies.py
+++ Server/api-courses/tecnologies.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from flask_cors import CORS
 from flask_mysqldb import MySQL
 import json
@@ -19,20 +19,37 @@ mysql = MySQL(app)
 @app.route('/')
 def index():
     cur = mysql.connection.cursor()
-    cur.execute("ALTER TABLE tecnologies CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci")
     cur.execute('SELECT * FROM tecnologies')
     datas = cur.fetchall()
     tecnologia = [{"NombreTecnologia":data[1]} for data in datas]
     
-    
     cur.execute('SELECT * FROM tecnologies')
     datos = cur.fetchall()
-    tecnologiades = [{"Descrippcion":dato[2]} for dato in datos]
+    tecdes = [{"Descripcion":dato[2]} for dato in datos]
     arr = []
-    arr.append([tecnologia, tecnologiades])
+    arr.append([tecnologia, tecdes])
     return (arr)
- 
- 
+
+
+
+
+@app.route('/section/<int:id>')
+def section(id):  
+    # id = request.args['id']
+    cur = mysql.connection.cursor()
+    cur.execute(f'SELECT * FROM section_description where id_section_description = {id}')
+    dates = cur.fetchall()
+    tectitle = [{"Titulo":date[2],"Informacion":date[3], "number": date[1], "id": date[0]} for date in dates]
+    
+    # cur = mysql.connection.cursor()
+    # cur.execute('SELECT * FROM videos')
+    # datas = cur.fetchall()
+    # video = [{"video":data[1]} for data in datas]
+    # print(id)
+    
+    # arr = []
+    # arr.append([tectitle,video])
+    return (tectitle) 
 
 if __name__ == '__main__':
     app.run(port = 3002, debug = True)
