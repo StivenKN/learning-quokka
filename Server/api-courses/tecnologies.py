b621662984791730ad5e1b17ad4356bbbcc2cd5a
alfons03 <mario.orozco.pacheco@gmail.com>
2023-09-28 08:40:47 -0500

    información

--- Server/api-courses/tecnologies.py
+++ Server/api-courses/tecnologies.py
@@ -1,28 +1,36 @@
-from flask import Flask, render_template, request, url_for, redirect, jsonify
+from flask import Flask, jsonify
+from flask_cors import CORS
 from flask_mysqldb import MySQL
 import json
 
  
 
 app = Flask(__name__)
+CORS(app)
+CORS(app, origins=['*'])
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
 app.config['MYSQL_PASSWORD'] = ''
 app.config['MYSQL_DB'] = 'tecnologie_data'
+app.config['MYSQL_CHARSET']='utf8mb4'
 mysql = MySQL(app)
 
 
 @app.route('/')
 def index():
     cur = mysql.connection.cursor()
+    cur.execute("ALTER TABLE tecnologies CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci")
     cur.execute('SELECT * FROM tecnologies')
     datas = cur.fetchall()
+    tecnologia = [{"NombreTecnologia":data[1]} for data in datas]
     
-    tecnologies = [{"id_tecnology": data[0], "name_tecnology": data[1], "description_tecnology": data[2]} for data in datas]
-    print (tecnologies)
     
-    jsonToSend = json.dumps(tecnologies, ensure_ascii=True)
-    return jsonify(jsonToSend)
+    cur.execute('SELECT * FROM tecnologies')
+    datos = cur.fetchall()
+    tecnologiades = [{"Descrippcion":dato[2]} for dato in datos]
+    arr = []
+    arr.append([tecnologia, tecnologiades])
+    return (arr)
  
  
 
