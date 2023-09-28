05205d4eb029d003f0579790d11554edc6ebe0b9
ConanGH-S <blandon0207s@gmail.com>
2023-09-28 11:06:11 -0500

    feat(courses): data tecnology courses UI

---
+++ Client/app/courses/[id]/test.tsx
@@ -0,0 +1,8 @@
+'use client'
+import { type ReactElement } from 'react'
+
+export const Consoletest = (data: any): ReactElement => {
+  console.log(data)
+  console.log('hola')
+  return <h2>hola</h2>
+}
